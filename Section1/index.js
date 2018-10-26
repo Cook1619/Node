52f199598851ed91575a5094271cc53e3cc85d5c
Cook1619 <cook6348@gmail.com>
2018-10-26 08:56:58 -0500

    taking user input and displaying input

--- Section1/index.js
+++ Section1/index.js
@@ -1,7 +1,28 @@
-let fs = require("fs");
+/*
+* Primary file for API
+*
+*/
+// Dependancies
+const http =  require('http');
+const url = require ('url');
 
-let jokes = {};
+// The server should respond to all requests with a string
+const server = http.createServer(function(req,res){
 
-jokes.allJokes = function() {
-  let fileContents = fs.readFileSync(__dirname);
-};
+  // Get the url and parse it
+  let parsedUrl = url.parse(req.url, true);
+
+  // Get the path of the url
+  let path = parsedUrl.pathname;
+  let trimmedPath =  path.replace(/^\/+|\/+$/g ,'')
+  // Send the response
+  res.end('Hello World\n')
+
+  // Log the request path
+  console.log(`Request is received on this path: ${trimmedPath}`)
+});
+
+// Start the server, and have it listen on port 3000
+server.listen(3000, function(){
+  console.log('Server is listening on port 3000 now')
+})
